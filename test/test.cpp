0ece1e9c3d7018e3ca9dea741aa91c4db3688d7e
DarthFame <mainfame@mail.ru>
2022-01-26 11:41:07 +0300

    Initial...

---
+++ test/test.cpp
@@ -0,0 +1,18 @@
+﻿
+#include "test.h"
+
+using namespace std;
+
+int main()
+{
+	char ch;
+	int num = 0;
+	double sq_root = 0;
+
+	for (num = 1; num < 100; num++) {
+		sq_root = sqrt((double)num);
+		cout << "Число: " << num << " " << "Квадратный корень: " << sq_root << '\n';
+	}
+	
+	return 0;
+}
